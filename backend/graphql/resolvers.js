e8ebf5e390b9d483d8ce30b0f1bcdebfb96fc7ac
Faiz <faizkhan8225@gmail.com>
2024-09-20 15:38:19 +0530

    Adding a mutation resolver and graphiQl

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -1,8 +1,25 @@
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user");
+
 module.exports = {
-    hello() {
-        return {
-            text: "Hello World",
-            views: 123
-        }
+  createUser: async function createUser({ userInput }, req) {
+    const existingUser = await User.findOne({ email: userInput.email });
+
+    if (existingUser) {
+      throw new Error("This email address already exist.");
     }
-}
+
+    const hashedPw = await bcrypt.hash(userInput.password, 12);
+
+    const user = new User({
+      email: userInput.email,
+      name: userInput.name,
+      password: hashedPw,
+    });
+
+    const createdUser = await user.save()
+    return { ...createdUser._doc, _id: createdUser._id.toString() }
+
+  },
+};
